cdfca328070bd363f7ca34261b7d3886f356048e
jeylanab <146985587+jeylanab@users.noreply.github.com>
2024-05-22 07:41:11 +0000

    ewfer

--- main.py
+++ main.py
@@ -2,3 +2,25 @@ print("hello world")
 print("second line")
 a = 66
 print(a)
+
+item = 'JEYLAN'
+Item = 'teesi'
+print(item, Item)
+
+# data types in python
+num = 5
+sex = 'male'
+isHappy = True
+listItem = ['good', 'bad', 'ugly', 55 ]
+
+print(num)
+print(sex)
+print(isHappy)
+print(listItem)
+
+isAdult = False
+
+if isAdult:
+  print('you are not old enough')
+else:
+  print('i am jeylan')
