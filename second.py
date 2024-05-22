f3675441eb4b1300fac61f6e55963b8cc8cde6c7
jeylanab <146985587+jeylanab@users.noreply.github.com>
2024-05-22 09:41:10 +0000

    added code for nd array

--- second.py
+++ second.py
@@ -10,3 +10,14 @@ print('this is my second file')
 course = 'python'
 print(len(course))
 
+# some built in functions of the numpy array
+
+arr3 = np.zeros((2,6))
+arr4 = np.ones((4,2))
+arr5 = np.full((2,2), 5)
+arange = np.arange(0, 10, 2)     # Array with values 0 to 8 with step 2
+print(arr3)
+print(arr4)
+print(arr5)
+print(arange)
+
